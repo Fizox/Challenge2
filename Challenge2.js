11992d3d349bbacf35e1ce93a7a5329af89b3995
Fizox <105630035+Fizox@users.noreply.github.com>
2023-03-28 16:19:25 +0300

    first commit

---
+++ Challenge2.js
@@ -0,0 +1,37 @@
+function findMajority(arr)
+{   
+    let n = arr.length;
+    let bool= true;
+    let bool2= false;
+    for (let index = 0; index < array.length; index++) {
+        if ((-10)**9 <= arr[i] <= 10**9) {
+            continue;
+        }else{
+            bool=false;
+            break;
+        }  
+        }
+    if(1 <= n <= 5*10**4 && bool===true){
+        for(let i = 0; i < n; i++) 
+        {
+            let count = 0;
+            for(let j = 0; j < n; j++) 
+                {
+                if (arr[i] === arr[j]){
+                    count++;
+                }
+                if (count>n/2) {
+                    console.log("the majority elemen is"+arr[i]);
+                    bool2=false;
+                    break;
+                }
+            }
+            if (bool2===true) {
+                break;
+            }
+        }
+    }else{
+        console.log("doesn't meet the recourment")
+    }
+}
+  
